38ce6e8782a2f623f95a55fd4784bd74d8701a4c
debojitkakoti <debojkakoti88@gmail.com>
2017-01-14 09:17:24 +0000

    Slack backend integration complete

--- bot/config.py
+++ bot/config.py
@@ -5,7 +5,7 @@ import logging
 # the options in the more complete config-template.py from here:
 # https://raw.githubusercontent.com/errbotio/errbot/master/errbot/config-template.py
 
-BACKEND = 'Text'  # Errbot will start in text mode (console only mode) and will answer commands from there.
+BACKEND = 'Slack'  # Errbot will start in text mode (console only mode) and will answer commands from there.
 
 BOT_DATA_DIR = r'/root/atraey/bot/data'
 BOT_EXTRA_PLUGIN_DIR = '/root/atraey/bot/plugins'
@@ -13,4 +13,68 @@ BOT_EXTRA_PLUGIN_DIR = '/root/atraey/bot/plugins'
 BOT_LOG_FILE = r'/root/atraey/bot/errbot.log'
 BOT_LOG_LEVEL = logging.DEBUG
 
-BOT_ADMINS = ('CHANGE ME', )  # !! Don't leave that to "CHANGE ME" if you connect your errbot to a chat system !!
+BOT_ADMINS = ('debojitkakoti88', )  # !! Don't leave that to "CHANGE ME" if you connect your errbot to a chat system !!
+
+# The identity, or credentials, used to connect to a server
+BOT_IDENTITY = {
+    # XMPP (Jabber) mode
+    # 'username': 'err@localhost',  # The JID of the user you have created for the bot
+    # 'password': 'changeme',       # The corresponding password for this user
+    # 'server': ('host.domain.tld',5222), # server override
+
+    ## HipChat mode (Comment the above if using this mode)
+    # 'username' : '12345_123456@chat.hipchat.com',
+    # 'password' : 'changeme',
+    ## Group admins can create/view tokens on the settings page after logging
+    ## in on HipChat's website
+    # 'token'    : 'ed4b74d62833267d98aa99f312ff04',
+    ## If you're using HipChat server (self-hosted HipChat) then you should set
+    ## the endpoint below. If you don't use HipChat server but use the hosted version
+    ## of HipChat then you may leave this commented out.
+    # 'endpoint' : 'https://api.hipchat.com'
+
+    ## Slack mode (comment the others above if using this mode)
+    'token': 'xoxb-127669813620-8XOT8qbh7ya45ItqgV087BBS',
+
+    ## Telegram mode (comment the others above if using this mode)
+    # 'token': '103419016:AAbcd1234...',
+
+    ## IRC mode (Comment the others above if using this mode)
+    # 'nickname' : 'err-chatbot',
+    # 'username' : 'err-chatbot',    # optional, defaults to nickname if omitted
+    # 'password' : None,             # optional
+    # 'server' : 'irc.freenode.net',
+    # 'port': 6667,                  # optional
+    # 'ssl': False,                  # optional
+    # 'ipv6': False,                 # optional
+    # 'nickserv_password': None,     # optional
+    ## Optional: Specify an IP address or hostname (vhost), and a
+    ## port, to use when making the connection. Leave port at 0
+    ## if you have no source port preference.
+    ##    example: 'bind_address': ('my-errbot.io', 0)
+    # 'bind_address': ('localhost', 0),
+}
+##########################################################################
+# Prefix configuration                                                   #
+##########################################################################
+
+# Command prefix, the prefix that is expected in front of commands directed
+# at the bot.
+#
+# Note: When writing plugins,you should always use the default '!'.
+# If the prefix is changed from the default, the help strings will be
+# automatically adjusted for you.
+#
+# BOT_PREFIX = '!'
+#
+# Uncomment the following and set it to True if you want the prefix to be
+# optional for normal chat.
+# (Meaning messages sent directly to the bot as opposed to within a MUC)
+#BOT_PREFIX_OPTIONAL_ON_CHAT = False
+
+# You might wish to have your bot respond by being called with certain
+# names, rather than the BOT_PREFIX above. This option allows you to
+# specify alternative prefixes the bot will respond to in addition to
+# the prefix above.
+BOT_ALT_PREFIXES = ('atraey',)
+
